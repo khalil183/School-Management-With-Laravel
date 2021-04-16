fdb5931c95c6718af50d8511fbd1c956332a9b75
khalil183 <aboutkhalil.83@gmail.com>
2021-04-16 22:02:59 +0600

    admin template mastering

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,24 +1,55 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Admin Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                    <a href="{{ route('admin.logout') }}">admin Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@extends('admin.layout')
+@section('admin-content')
+              <!-- Main content -->
+              <section class="content">
+                <div class="row">
+                  <div class="col-xs-12">
+                    <div class="box">
+                      <div class="box-header">
+                        <h3 class="box-title">Data Table With Full Features</h3>
+                      </div><!-- /.box-header -->
+                      <div class="box-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                          <thead>
+                            <tr>
+                              <th>Rendering engine</th>
+                              <th>Browser</th>
+                              <th>Platform(s)</th>
+                              <th>Engine version</th>
+                              <th>CSS grade</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>Trident</td>
+                              <td>Internet
+                                Explorer 4.0</td>
+                              <td>Win 95+</td>
+                              <td> 4</td>
+                              <td>X</td>
+                            </tr>
+                            <tr>
+                              <td>Other browsers</td>
+                              <td>All others</td>
+                              <td>-</td>
+                              <td>-</td>
+                              <td>U</td>
+                            </tr>
+                          </tbody>
+                          <tfoot>
+                            <tr>
+                              <th>Rendering engine</th>
+                              <th>Browser</th>
+                              <th>Platform(s)</th>
+                              <th>Engine version</th>
+                              <th>CSS grade</th>
+                            </tr>
+                          </tfoot>
+                        </table>
+                      </div><!-- /.box-body -->
+                    </div><!-- /.box -->
+                  </div><!-- /.col -->
+                </div><!-- /.row -->
+              </section><!-- /.content -->
+            </div><!-- /.content-wrapper -->
 @endsection
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,104 @@
+@include('partials.header')
+
+    <div class="wrapper">
+
+        <header class="main-header">
+          <!-- Logo -->
+          <a href="../../index2.html" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>A</b>LT</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Admin</b>LTE</span>
+          </a>
+          <!-- Header Navbar: style can be found in header.less -->
+          <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+          </nav>
+        </header>
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+          <!-- sidebar: style can be found in sidebar.less -->
+          <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+              <div class="pull-left image">
+                <img src="{{ asset('public/admin/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image" />
+              </div>
+              <div class="pull-left info">
+                <p>Alexander Pierce</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+              </div>
+            </div>
+
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu">
+              <li class="header">MAIN NAVIGATION</li>
+              <li><a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+              <li class="treeview">
+                <a href="#">
+                  <i class="fa fa-th"></i> <span>Setup School</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                  <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                </ul>
+              </li>
+              <li class="treeview">
+                <a href="#">
+                  <i class="fa fa-files-o"></i>
+                  <span>Layout Options</span>
+                  <span class="label label-primary pull-right">4</span>
+                </a>
+                <ul class="treeview-menu">
+                  <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                  <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
+                  <li><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
+                  <li><a href="../layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="../widgets.html">
+                  <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
+                </a>
+              </li>
+              <li><a href="../../documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+              <li><a href="{{ route('admin.logout') }}"><i class="fa fa-book"></i> <span>Admin Logout</span></a></li>
+
+
+            </ul>
+          </section>
+          <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+          <!-- Content Header (Page header) -->
+          <section class="content-header">
+            <h1>
+              Data Tables
+              <small>advanced tables</small>
+            </h1>
+            <ol class="breadcrumb">
+              <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+              <li><a href="#">Tables</a></li>
+              <li class="active">Data tables</li>
+            </ol>
+          </section>
+
+          @yield('admin-content')
+        <footer class="main-footer">
+          <div class="pull-right hidden-xs">
+            <b>Version</b> 2.2.0
+          </div>
+          <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        </footer>
+      </div><!-- ./wrapper -->
+
+      @include('partials.footer')
+
